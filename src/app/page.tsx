1dd7becf8781726ac75d3aaecec470357486d7ee
Ayomide Oguntuase <oguntuase3@gmail.com>
2025-07-11 15:06:43 +0100

    initial commit for grid blocks and name animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,297 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+
+const AMOCoordinates: { x: number; y: number; z: number }[] = [
+  { x: -10, y: 0, z: 7 },
+  { x: -10, y: 0, z: 6 },
+  { x: -10, y: 0, z: 5 },
+  { x: -10, y: 0, z: 4 },
+];
+
+const gridCellWidth = 50;
+
+const planeWidth = 1000;
+
+const timeInterval = 500;
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas || typeof window === undefined) return;
+
+    let camera: THREE.PerspectiveCamera;
+    let scene: THREE.Scene;
+    let renderer: THREE.WebGLRenderer;
+    let pointer: THREE.Vector2;
+    let raycaster: THREE.Raycaster;
+    let isShiftDown = false;
+
+    let rollOverMesh: THREE.Mesh;
+    let rollOverMaterial: THREE.MeshBasicMaterial;
+    let cubeGeo: THREE.BoxGeometry;
+    let cubeMaterial: THREE.MeshLambertMaterial;
+    let plane: THREE.Mesh;
+    const objects: THREE.Mesh[] = [];
+
+    const init = () => {
+      camera = new THREE.PerspectiveCamera(
+        45,
+        window.innerWidth / window.innerHeight,
+        1,
+        10000
+      );
+      camera.position.set(500, 800, 1300);
+      camera.lookAt(0, 0, 0);
+
+      scene = new THREE.Scene();
+      scene.background = new THREE.Color(0xf0f0f0);
+
+      // roll-over helpers
+
+      const rollOverGeo = new THREE.BoxGeometry(50, 50, 50);
+      rollOverMaterial = new THREE.MeshBasicMaterial({
+        color: 0xff0000,
+        opacity: 0.5,
+        transparent: true,
+      });
+      rollOverMesh = new THREE.Mesh(rollOverGeo, rollOverMaterial);
+      scene.add(rollOverMesh);
+
+      // cubes
+
+      const map = new THREE.TextureLoader().load(
+        "/textures/square-outline-textured.png"
+      );
+      map.colorSpace = THREE.SRGBColorSpace;
+      cubeGeo = new THREE.BoxGeometry(50, 50, 50);
+      cubeMaterial = new THREE.MeshLambertMaterial({
+        color: 0xfeb74c,
+        map: map,
+      });
+
+      // grid
+
+      const gridHelper = new THREE.GridHelper(
+        planeWidth,
+        planeWidth / gridCellWidth
+      );
+      scene.add(gridHelper);
+
+      raycaster = new THREE.Raycaster();
+      pointer = new THREE.Vector2();
+
+      const geometry = new THREE.PlaneGeometry(planeWidth, planeWidth);
+      geometry.rotateX(-Math.PI / 2);
+
+      plane = new THREE.Mesh(
+        geometry,
+        new THREE.MeshBasicMaterial({ visible: false })
+      );
+      scene.add(plane);
+
+      objects.push(plane);
+
+      // lights
+
+      const ambientLight = new THREE.AmbientLight(0x606060, 3);
+      scene.add(ambientLight);
+
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
+      directionalLight.position.set(1, 0.75, 0.5).normalize();
+      scene.add(directionalLight);
+
+      renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      // document.body.appendChild(renderer.domElement);
+
+      document.addEventListener("pointermove", onPointerMove);
+      document.addEventListener("pointerdown", onPointerDown);
+      document.addEventListener("keydown", onDocumentKeyDown);
+      document.addEventListener("keyup", onDocumentKeyUp);
+
+      //
+
+      window.addEventListener("resize", onWindowResize);
+    };
+
+    function onWindowResize() {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize(window.innerWidth, window.innerHeight);
+
+      render();
+    }
+
+    function onPointerMove(event: PointerEvent) {
+      const x = (event.clientX / window.innerWidth) * 2 - 1;
+      const y = -(event.clientY / window.innerHeight) * 2 + 1;
+      pointer.set(x, y);
+
+      raycaster.setFromCamera(pointer, camera);
+
+      const intersects = raycaster.intersectObjects(objects, false);
+
+      if (intersects.length > 0) {
+        const intersect = intersects[0];
+        if (!intersect.face) return;
+
+        rollOverMesh.position.copy(intersect.point).add(intersect.face.normal);
+        rollOverMesh.position
+          .divideScalar(gridCellWidth)
+          .floor()
+          .multiplyScalar(gridCellWidth)
+          .addScalar(gridCellWidth / 2);
+
+        render();
+      }
+    }
+
+    function onPointerDown(event: PointerEvent) {
+      // calculate pointer position in normalized device coordinates
+      const x = (event.clientX / window.innerWidth) * 2 - 1;
+      const y = -(event.clientY / window.innerHeight) * 2 + 1;
+      console.log({ x, y });
+      pointer.set(x, y);
+
+      raycaster.setFromCamera(pointer, camera);
+
+      const intersects = raycaster.intersectObjects(objects, false);
+
+      if (intersects.length > 0) {
+        const intersect = intersects[0];
+        if (!intersect.face) return;
+
+        console.log({ intersect });
+
+        // delete cube
+        if (isShiftDown) {
+          if (intersect.object !== plane) {
+            scene.remove(intersect.object);
+
+            objects.splice(objects.indexOf(intersect.object), 1);
+          }
+
+          // create cube
+        } else {
+          const voxel = new THREE.Mesh(cubeGeo, cubeMaterial);
+          voxel.position.copy(intersect.point).add(intersect.face.normal);
+          voxel.position
+            .divideScalar(gridCellWidth)
+            .floor()
+            .multiplyScalar(gridCellWidth)
+            .addScalar(gridCellWidth / 2);
+          scene.add(voxel);
+
+          objects.push(voxel);
+        }
+
+        render();
+      }
+    }
+
+    function onDocumentKeyDown(event: KeyboardEvent) {
+      switch (event.keyCode) {
+        case 16:
+          isShiftDown = true;
+          break;
+      }
+    }
+
+    function onDocumentKeyUp(event: KeyboardEvent) {
+      switch (event.keyCode) {
+        case 16:
+          isShiftDown = false;
+          break;
+      }
+    }
+
+    function render() {
+      renderer.render(scene, camera);
+    }
+
+    init();
+
+    const animatedCoordinates: (typeof AMOCoordinates)[number][] = [];
+
+    const animateName = () => {
+      const addName = () => {
+        AMOCoordinates.forEach((coordinate, index) => {
+          const voxel = new THREE.Mesh(cubeGeo, cubeMaterial);
+          voxel.position.set(
+            coordinate.x * gridCellWidth,
+            coordinate.y * gridCellWidth,
+            coordinate.z * gridCellWidth
+          );
+          voxel.position
+            .divideScalar(gridCellWidth)
+            .floor()
+            .multiplyScalar(gridCellWidth)
+            .addScalar(gridCellWidth / 2);
+          setTimeout(() => {
+            scene.add(voxel);
+            render();
+          }, timeInterval * index);
+
+          objects.push(voxel);
+          animatedCoordinates.unshift(coordinate);
+        });
+      };
+
+      console.log({ animatedCoordinates });
+
+      const removeName = () => {
+        animatedCoordinates.forEach((coordinate, index) => {
+          const voxel = objects.find((object) => {
+            const position = new THREE.Vector3(
+              coordinate.x * gridCellWidth,
+              coordinate.y * gridCellWidth,
+              coordinate.z * gridCellWidth
+            )
+              .divideScalar(gridCellWidth)
+              .ceil()
+              .multiplyScalar(gridCellWidth)
+              .addScalar(gridCellWidth / 2);
+
+            console.log({ position });
+
+            return object.position.equals(position);
+          });
+          console.log({ voxel });
+          if (voxel) {
+            setTimeout(() => {
+              scene.remove(voxel);
+              objects.splice(objects.indexOf(voxel), 1);
+              render();
+            }, timeInterval * index);
+          }
+        });
+      };
+
+      addName();
+
+      setTimeout(() => {
+        removeName();
+      }, timeInterval * AMOCoordinates.length);
+    };
+
+    setTimeout(() => {
+      animateName();
+    }, timeInterval);
+
+    render();
+
+    console.log({ objects });
+  }, []);
+
+  return (
+    <div className="w-screen h-screen">
+      <canvas ref={canvasRef} className="w-full h-full" />
     </div>
   );
 }
